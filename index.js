44f47fccaa95791ee404a3744eeab91a8c402a60
suresh524 <suresharumulla.a@gmail.com>
2019-06-15 16:30:35 +0530

    restapi

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+//set up express app
+const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
+
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+//initialize routes
+app.use('/api', require('./routes/api'));
+
+//error handling middleware
+// app.use(function (err, req, res, next) {
+//     //console.log(err);
+//     res.status(422).send({
+//         error: err.message
+//     });
+// })
+
+//listen  for requests
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('now listening for requests')
+})
